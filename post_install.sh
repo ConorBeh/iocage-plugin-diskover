5094386509aae9b367e36f9277340050c6ff96db
ConorBeh <rogerairgood@gmail.com>
2020-02-04 16:48:22 -0800

    Update post_install.sh

--- post_install.sh
+++ post_install.sh
@@ -245,4 +245,4 @@ echo "A daily crontab has been installed to index the storage pool. If time peri
 echo "Installing Diskover..." >> /root/PLUGIN_INFO
 echo "After installation, please mount your pool inside the jail to /mnt/fnpool" >> /root/PLUGIN_INFO
 echo "A daily crontab has been installed to index the storage pool. If time period is not desirable, enter the jail and run "crontab -e" to modify." >> /root/PLUGIN_INFO
-
+exit
